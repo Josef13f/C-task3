faa9f5ba14cd3d9ac0e7df5963b62e276d732c97
Jose <13work13121981@gmail.com>
2024-02-26 23:56:53 +0700

    added task3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте произвольный массив. Выведете
+//его элементы, начиная с конца. Использовать
+//рекурсию, не использовать циклы.
+// [5, 6, 8, 7, 3, 0] => 5 6 8 7 3 0
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[] { 5, 6, 8, 7, 3, 0 };
+        PrintReverseArray(array, array.Length - 1);
+    }
+
+    static void PrintReverseArray(int[] array, int j)
+    {
+        if (j < 0) 
+        {
+            return;
+        }
+
+        Console.Write(array[j] + " "); 
+
+        PrintReverseArray(array, j - 1); 
+    }
+}
